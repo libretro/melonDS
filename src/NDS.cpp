c20ddfd3a90ea2d13c5d5a1f39ebb31e2903d063
Stuart Kenny <stuartkenny@gmail.com>
2021-09-28 20:33:21 +0100

    Rollback incorrect upstream changes.

--- src/NDS.cpp
+++ src/NDS.cpp
@@ -89,7 +89,6 @@ u64 FrameStartTimestamp;
 int CurCPU;
 
 const s32 kMaxIterationCycles = 64;
-const s32 kIterationCycleMargin = 8;
 
 u32 ARM9ClockShift;
 
@@ -915,7 +914,7 @@ void RelocateSave(const char* path, bool write)
 
 u64 NextTarget()
 {
-    u64 minEvent = UINT64_MAX;
+    u64 ret = SysTimestamp + kMaxIterationCycles;
 
     u32 mask = SchedListMask;
     for (int i = 0; i < Event_MAX; i++)
@@ -923,19 +922,14 @@ u64 NextTarget()
         if (!mask) break;
         if (mask & 0x1)
         {
-            if (SchedList[i].Timestamp < minEvent)
-                minEvent = SchedList[i].Timestamp;
+            if (SchedList[i].Timestamp < ret)
+                ret = SchedList[i].Timestamp;
         }
 
         mask >>= 1;
     }
 
-    u64 max = SysTimestamp + kMaxIterationCycles;
-
-    if (minEvent < max + kIterationCycleMargin)
-        return minEvent;
-
-    return max;
+    return ret;
 }
 
 void RunSystem(u64 timestamp)
@@ -972,6 +966,7 @@ u32 RunFrame()
 
         while (Running && GPU::TotalScanlines==0)
         {
+            // TODO: give it some margin, so it can directly do 17 cycles instead of 16 then 1
             u64 target = NextTarget();
             ARM9Target = target << ARM9ClockShift;
             CurCPU = 0;
